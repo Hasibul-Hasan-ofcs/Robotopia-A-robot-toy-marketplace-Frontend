005c825cf156708f3ba1dd109e20d29b08375734
MD-Hasibul_Hasan <hasibulofcs@gmail.com>
2023-05-20 19:48:03 +0600

    tabbed component updates

--- src/components/ShopByCategory.jsx
+++ src/components/ShopByCategory.jsx
@@ -2,42 +2,12 @@ import React, { useState } from "react";
 import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import "../css/shopByCategory.css";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 import { subCategoryData } from "../js/jsonLoader";
+import { Link } from "react-router-dom";
 
 const ShopByCategory = () => {
   const [tabIndex, setTabIndex] = useState(0);
-
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    centerMode: false,
-    arrows: true,
-    variableWidth: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          centerMode: false,
-          variableWidth: false,
-        },
-      },
-      {
-        breakpoint: 1024, // Tablet breakpoint
-        settings: {
-          slidesToShow: 2,
-          centerMode: false,
-        },
-      },
-    ],
-  };
 
   return (
     <div className="py-5" id="gallery">
@@ -52,71 +22,65 @@ const ShopByCategory = () => {
             <Tab>Transformers </Tab>
             <Tab>Macross </Tab>
           </TabList>
-          <TabPanel className="dark_bg01">
-            <Slider {...settings}>
-              {subCategoryData.map((el, indx) => {
-                return (
-                  el.type === "g" && (
-                    <div
-                      className="card m-4 p-3 d-flex gap-2 flex-column"
-                      key={indx}
-                    >
-                      <img src={el.img} className="h-250 rounded shp-img" />
-                      <h4 className="text-white pt-3">{el.name}</h4>
-                      <h2 className="theme-color">{el.price}$</h2>
-                      <p>rating: {el.rating}</p>
-                      <div className="theme-button rounded text-center">
-                        View Details
-                      </div>
-                    </div>
-                  )
-                );
-              })}
-            </Slider>
+          <TabPanel className="dark_bg01 rounded d-flex flex-wrap gap-4 justify-content-center">
+            {subCategoryData.map((el, indx) => {
+              return (
+                el.type === "g" && (
+                  <div
+                    className="card m-4 p-3 d-flex gap-2 flex-column"
+                    key={indx}
+                  >
+                    <img src={el.img} className="h-250 rounded shp-img" />
+                    <h4 className="text-white pt-3">{el.name}</h4>
+                    <h2 className="theme-color">{el.price}$</h2>
+                    <p>rating: {el.rating}</p>
+                    <Link className="theme-button rounded text-center">
+                      View Details
+                    </Link>
+                  </div>
+                )
+              );
+            })}
           </TabPanel>
-          <TabPanel className="dark_bg01">
-            <Slider {...settings}>
-              {subCategoryData.map((el, indx) => {
-                return (
-                  el.type === "t" && (
-                    <div
-                      className="card m-4 p-3 d-flex gap-2 flex-column"
-                      key={indx}
-                    >
-                      <img src={el.img} className="h-250 rounded shp-img" />
-                      <h4 className="text-white pt-3">{el.name}</h4>
-                      <h2 className="theme-color">{el.price}$</h2>
-                      <p>rating: {el.rating}</p>
-                      <div className="theme-button rounded text-center">
-                        View Details
-                      </div>
-                    </div>
-                  )
-                );
-              })}
-            </Slider>
+          <TabPanel className="dark_bg01 rounded d-flex flex-wrap gap-4 justify-content-center">
+            {subCategoryData.map((el, indx) => {
+              return (
+                el.type === "t" && (
+                  <div
+                    className="card m-4 p-3 d-flex gap-2 flex-column"
+                    key={indx}
+                  >
+                    <img src={el.img} className="h-250 rounded shp-img" />
+                    <h4 className="text-white pt-3">{el.name}</h4>
+                    <h2 className="theme-color">{el.price}$</h2>
+                    <p>rating: {el.rating}</p>
+                    <Link className="theme-button rounded text-center">
+                      View Details
+                    </Link>
+                  </div>
+                )
+              );
+            })}
           </TabPanel>
-          <TabPanel className="dark_bg01">
-            <Slider {...settings}>
-              {subCategoryData.map((el, indx) => {
-                return (
-                  el.type === "m" && (
-                    <div
-                      className="card m-4 p-3 d-flex gap-2 flex-column"
-                      key={indx}
-                    >
-                      <img src={el.img} className="h-250 rounded shp-img" />
-                      <h4 className="text-white pt-3">{el.name}</h4>
-                      <h2 className="theme-color">{el.price}$</h2>
-                      <p>rating: {el.rating}</p>
-                      <div className="theme-button rounded text-center">
-                        View Details
-                      </div>
-                    </div>
-                  )
-                );
-              })}
-            </Slider>
+          <TabPanel className="dark_bg01 rounded d-flex flex-wrap gap-4 justify-content-center">
+            {subCategoryData.map((el, indx) => {
+              return (
+                el.type === "m" && (
+                  <div
+                    className="card m-4 p-3 d-flex gap-2 flex-column"
+                    key={indx}
+                  >
+                    <img src={el.img} className="h-250 rounded shp-img" />
+                    <h4 className="text-white pt-3">{el.name}</h4>
+                    <h2 className="theme-color">{el.price}$</h2>
+                    <p>rating: {el.rating}</p>
+                    <Link className="theme-button rounded text-center">
+                      View Details
+                    </Link>
+                  </div>
+                )
+              );
+            })}
           </TabPanel>
         </Tabs>
       </div>
